928a726ced50f12a4bc682909f177aa7c994d528
MichealJou <864738795@qq.com>
2015-12-22 13:38:03 +0800

    提交代码
    提交代码

--- Week07/baidu/js/main.js
+++ Week07/baidu/js/main.js
@@ -1,24 +1,26 @@
 (function($) {
-
     $("[data-show='show']").each(function(index, elments) {
         $(elments).hover(function() {
             var _this = $(this);
             var showId = _this.data("show-id");
             $("#" + showId).show();
         }, function(e) {
-
             var _this = $(this);
             var showId = _this.data("show-id");
             var showIdDiv = $("#" + showId);
             $(showIdDiv).hide();
-            $(showIdDiv).mouseover(function(){
+            $(showIdDiv).mouseover(function() {
                 $(showIdDiv).show();
             });
-           $(showIdDiv).mouseout(function(){
+            $(showIdDiv).mouseout(function() {
                 $(showIdDiv).hide();
             });
         });
-
     });
 
+  
+
 })(jQuery);
+
+
+//检查用户是否登录
